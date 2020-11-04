4bab44a86f2e660d930b8aee39b10097aef4c663
masset151 <48015763+masset151@users.noreply.github.com>
2020-11-04 20:59:21 +0100

    Terminado

--- Sustituye_Patrones.py
+++ Sustituye_Patrones.py
@@ -1,17 +1,30 @@
+import re
 fichero = "sustituciones.txt"
 frase = "1 me dijo que 0 vendría con 2"
 def sustituye_patrones(frase, fichero):
+    numeros = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
     with open(fichero, "r") as f:
         fichero = f.read()
-    nombres = [i for i in fichero.split()]
-    nombres = "".join(nombres)
-    nombres = nombres.split(":")
+    nombres = [i.rstrip("\n") for i in fichero.split(":")]
+
+    #linea = nombres[0].split(":")
+    #linea = linea.pop()
+    #print(linea)
+    #nombres = nombres.split(":")
 
     nombres = [i for i in nombres if i]
 
-    frase = frase.replace("0",nombres[0])
-    frase = frase.replace("1", nombres[1])
-    frase = frase.replace("2", nombres[2])
-    return print(frase)
 
-sustituye_patrones(frase,fichero)
+
+    for i in numeros:
+        for x in frase:
+            if x.isdigit():
+                num = x.isdigit()
+                frase = frase.replace(str(i),nombres[i])
+
+    num = len(nombres)
+    return frase
+
+print(sustituye_patrones(frase,fichero))
+print(sustituye_patrones("Los hijos de 4 son 3 y 5",fichero))
