5fa29cbb142a4c16489864d4e4a4559f5048aa24
Titomelen <javiermelendez744@gmail.com>
2020-11-05 19:04:45 +0100

    Subido todo lo de Individual bien

---
+++ Individual/Javier Melendez/Ejercicio4.py
@@ -0,0 +1,37 @@
+# -----------
+# EJERCICIO 4
+# -----------
+# Un número es perfecto si es la suma de todos sus divisores (excepto él
+# mismo). Definir una función filtra_perfectos(n,m,p) que imprime por pantalla
+# todos los números perfectos entre n y m que cumplen la condición p. Se pide
+# también que se indiquen los divisores de cada número perfecto que se
+# imprima.
+
+# Ejemplo:
+
+# >>> filtra_perfectos(3,500, lambda x: True)
+# El 6 es perfecto y sus divisores son [1, 2, 3]
+# El 28 es perfecto y sus divisores son [1, 2, 4, 7, 14]
+# El 496 es perfecto y sus divisores son [1, 2, 4, 8, 16, 31, 62, 124, 248]
+
+# >>> filtra_perfectos(3,500, lambda x: (x%7==0))
+# El 28 es perfecto y sus divisores son [1, 2, 4, 7, 14]
+# ------------------------------------------------------------------------
+
+def divisores(x):
+    div = [i for i in range(1,x) if x%i == 0]
+    return div
+
+def filtra_perfectos(a,b,f):
+    for x in range(a,b+1):
+        if sum(divisores(x)) == x:
+            if f(x):
+                print ( "El " , x , " es perfecto y sus divisores son " , divisores(x))
+
+print(filtra_perfectos(3,500, lambda x: True))
+print(filtra_perfectos(3,500, lambda x: (x%7==0)))
+
+##    for x in range(a,b+1):
+##        if sum(multiplos(x)) == x:
+##            if f(x):
+##                print ( "El " , x , " es perfecto y sus divisores son " , multiplos(x))
