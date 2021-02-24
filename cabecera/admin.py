57aba3d8bf7e3ae3a5198c4030767b25883b0990
titomelen <javiermelendez744@gmail.com>
2021-02-24 18:23:20 +0100

    Corregido

--- cabecera/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from cabecera.models import Cabecera
-# Register your models here.
-admin.site.register(Cabecera)
--- chats/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from usuario.models import Usuario
-
-# Create your models here.
-
-class Chat(models.Model):
-    id_chat = models.IntegerField(primary_key=True)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    mensaje = models.CharField(max_length=250)
